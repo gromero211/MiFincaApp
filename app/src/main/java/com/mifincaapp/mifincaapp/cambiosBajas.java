c761da8145f3b6af9ebb81ee75d1203aad9edb10
gkatia <g.ekaterina16@gmail.com>
2016-11-20 23:28:01 -0600

    cambios fragments

--- app/src/main/java/com/mifincaapp/mifincaapp/cambiosBajas.java
+++ app/src/main/java/com/mifincaapp/mifincaapp/cambiosBajas.java
@@ -1,13 +1,30 @@
 package com.mifincaapp.mifincaapp;
 
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class cambiosBajas extends AppCompatActivity {
+public class cambiosBajas extends Fragment {
 
+    public cambiosBajas() {
+
+    }
+
+    public static cambiosBajas getInstance(){
+        cambiosBajas fragment=new cambiosBajas();
+        return fragment;
+
+    }
+
+    @Nullable
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cambios_bajas);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.activity_cambios_bajas,container,false);
+        return view;
+
     }
 }
--- app/src/main/java/com/mifincaapp/mifincaapp/cambios_reidentificacion.java
+++ app/src/main/java/com/mifincaapp/mifincaapp/cambios_reidentificacion.java
@@ -23,7 +23,7 @@ public class cambios_reidentificacion extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view=inflater.inflate(R.layout.fragment_control_peso_registrar,container,false);
+        View view=inflater.inflate(R.layout.activity_cambios_reidentificacion,container,false);
         return view;
 
     }
