f1530c8395ec4c43550b3562b37842138644912d
gkatia <g.ekaterina16@gmail.com>
2016-12-03 17:31:35 -0600

    bajas

--- app/src/main/java/com/mifincaapp/mifincaapp/bajas_listado.java
+++ app/src/main/java/com/mifincaapp/mifincaapp/bajas_listado.java
@@ -1,7 +1,9 @@
 package com.mifincaapp.mifincaapp;
 
+import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -13,24 +15,24 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.mifincaapp.mifincaapp.R;
 import com.mifincaapp.mifincaapp.db.Db_inventario;
 
 import java.util.ArrayList;
 import java.util.List;
 
-
-public class Inventario_listado extends Fragment {
+public class bajas_listado extends Fragment {
     View view;
     ListView lista;
     Db_inventario db;
     SQLiteDatabase data;
     List<String> item = null;
     TextView tv_inId, tv_inArete;
-    public Inventario_listado() {
+    public bajas_listado() {
         // Required empty public constructor
     }
-    public static Inventario_listado getInstance(){
-        Inventario_listado fragment=new Inventario_listado();
+    public static bajas_listado getInstance(){
+        bajas_listado fragment=new bajas_listado();
         return fragment;
 
     }
@@ -54,14 +56,14 @@ public class Inventario_listado extends Fragment {
         String id="",fecha="",arete="",raza="";
 
 
-            //recorremos los registros del objeto cursor C
-           while (c.moveToNext()){
-               id=c.getString(0);
-                fecha=c.getString(1);
-                arete=c.getString(1);
-                raza=c.getString(3);
-                item.add(arete);
-            }
+        //recorremos los registros del objeto cursor C
+        while (c.moveToNext()){
+            id=c.getString(0);
+            fecha=c.getString(1);
+            arete=c.getString(1);
+            raza=c.getString(3);
+            item.add(arete);
+        }
 
 
         //crear el adapter de tipo list adapter
@@ -74,10 +76,10 @@ public class Inventario_listado extends Fragment {
                 //muestra el nuevo fragment
                 //
                 //na.setText("Población de "+ lv1.getItemAtPosition(posicion) + " es "+ habitantes[posicion]);
-                 TextView tv1=(TextView)view.findViewById(R.id.textView11);
+                TextView tv1=(TextView)view.findViewById(R.id.textView11);
                 String aret= (String) lista.getItemAtPosition(position);
 //                tv1.setText("posicion: "+lista.getItemIdAtPosition(position)+ " es " );
-               Toast.makeText(getContext(), aret, Toast.LENGTH_SHORT).show();
+                Toast.makeText(getContext(), aret, Toast.LENGTH_SHORT).show();
                 Db_inventario dbInventario = new Db_inventario(getActivity());
                 data = dbInventario.getWritableDatabase();
 
@@ -89,6 +91,4 @@ public class Inventario_listado extends Fragment {
             }
         });
     }
-
-
 }
